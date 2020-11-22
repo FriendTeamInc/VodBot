313b9ea4a3fea3e0563067ec7f709a0450e781a3
Logan H-D <notquiteapex@gmail.com>
2020-11-22 16:48:30 -0500

    Finish authorization, start on polling user-id's

--- vodbot/__main__.py
+++ vodbot/__main__.py
@@ -1,32 +1,59 @@
-from .util import exit_prog
+from . import util
 
 import os
+import requests
+import toml
 from pathlib import Path
 
 def main():
-	vodbotdir = str(Path.home() / ".vodbot")
+	vodbotdir = Path.home() / ".vodbot"
 
-	if not os.path.exists(vodbotdir):
-		if not os.path.isdir(vodbotdir):
-			exit_prog(54, f"Non-directory object \"{vodbotdir}\" must be removed before proceeding!")
+	if not os.path.exists(str(vodbotdir)):
+		util.make_dir(vodbotdir)
+
+		util.make_conf(vodbotdir)
 		
-		os.mkdir(vodbotdir)
+		util.exit_prog(1, f"Edit the config files in \"{vodbotdir}\" before running again.")
+	
+	if not os.path.isdir(str(vodbotdir)):
+		util.exit_prog(54, f"Non-directory object \"{vodbotdir}\" must be removed before proceeding!")
 
-		basetoml = {
-			"twitch": {
-				"client-id":"PUT TWITCH CLIENT-ID HERE",
-				"watch-channels":["notquiteapex","juicibox","batkigu"]
-			}
-		}
+	conf = None
+	try:
+		conf = toml.load(str(Path.home() / ".vodbot" / "conf.toml"))
+	except FileNotFoundError:
+		util.make_conf(vodbotdir)
+		util.exit_prog(2, f"Config not found. New one has been made at \"{vodbotdir}\".")
+	
+	CLIENT_ID = conf["twitch"]["client-id"]
+	CLIENT_SECRET = conf["twitch"]["client-secret"]
+	CHANNELS = conf["twitch"]["channels"]
 
-	# GET https://api.twitch.tv/helix/users: get User-IDs with this, using the watch channels list
+	if CLIENT_ID == util.defaultclientid or CLIENT_SECRET == util.defaultclientsecret:
+		util.exit_prog(3, f"Please edit your config with your Client ID and Secret.")
+
+	clientsecreturl = "https://id.twitch.tv/oauth2/token?"
+	clientsecreturl += f"client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&"
+	clientsecreturl += "grant_type=client_credentials"
+	accesstokenrequest = requests.post(clientsecreturl)
+
+	accesstoken = accesstokenrequest.json()
+
+	if "access_token" in accesstoken:
+		accesstoken = accesstoken["access_token"]
+	else:
+		util.exit_prog(4, "Could not get access token! Check your Client ID/Secret.")
+
+	headers = {"Client-ID":CLIENT_ID, "Authorization": "Bearer " + accesstoken}
+
+	userids = []
+	getidsurl = "https://api.twitch.tv/helix/users?" 
+	getidsurl += "&".join(f"login={i}" for i in conf["twitch"]["channels"])
+	print(getidsurl)
+
 	# GET https://api.twitch.tv/helix/videos: get videos using the IDs
 
-	try:
-		pass
-	except FileNotFoundError:
-		pass
-	print("Hello, World!")
+	#print(accesstokenrequest.json())
 
 
 if __name__ == "__main__":
--- vodbot/util.py
+++ vodbot/util.py
@@ -1,4 +1,27 @@
+import os
 import sys
+import toml
+from pathlib import Path
+
+defaultclientid = "TWITCH.CLIENT-ID"
+defaultclientsecret = "TWITCH.CLIENT-SECRET"
+
+def make_dir(directory):
+	os.mkdir(str(directory))
+
+def make_conf(directory):
+	basetoml = {
+		"twitch": {
+			"client-id":defaultclientid,
+			"client-secret":defaultclientsecret,
+			"channels":["notquiteapex","juicibox","batkigu"]
+		}
+	}
+
+	filedata = toml.dumps(basetoml)
+
+	with open(str(directory / "conf.toml"), "w") as f:
+		f.write(filedata)
 
 def exit_prog(code=0, errmsg=None):
 	if code != 0:
