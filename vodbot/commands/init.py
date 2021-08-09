b5e949f604c3af46de6813161c931b51ed05209e
Logan H-D <self@notquiteapex.net>
2021-08-09 08:33:50 -0400

    outline prints

--- vodbot/commands/init.py
+++ vodbot/commands/init.py
@@ -1,7 +1,29 @@
 # Initialization of VodBot, setting up configs, etc.
 
 import vodbot.util as util
+import vodbot.printer as cp
 
 def run(args):
-	# TODO: should run a bunch of input() prompts asking for info to set up VodBot
 	pass
+	# TODO: should run a bunch of input() prompts asking for info to set up VodBot
+
+	# Ask for channels
+	cp.cprint("Enter the login names of the Twitch channels you'd like to have archived.")
+	cp.cprint("Example: the url is `https://twitch.tv/notquiteapex`, enter `notquiteapex`")
+	while True:
+		break
+
+	# Ask for timezone (list out some common ones from pytz)
+	cp.cprint("Enter the timezone for timedate referencing.")
+
+	# ask for directories (have defaults ready)
+	cp.cprint("Enter where VODs should be stored. Default (``)")
+	cp.cprint("Enter where Clips should be stored. Default (``)")
+	cp.cprint("Enter where temporary data should be stored. Default (``)")
+	cp.cprint("Enter where staged data should be stored. Default (``)")
+
+	# ready to write it all, go!
+	cp.cprint("Writing config...")
+
+	# list the location of the config and say what can be edited outside this command
+	cp.cprint("Finished, the config can be edited at ``.")
