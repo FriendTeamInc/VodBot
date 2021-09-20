3005819a80cabd25059cb7478f81512d4e6defcd
Logan H-D <notquiteapex@gmail.com>
2021-09-20 00:43:13 -0400

    finish concat

--- vodbot/video.py
+++ vodbot/video.py
@@ -4,6 +4,7 @@ from . import util
 from .printer import cprint
 from .commands.stage import StageData, VideoSlice
 
+import os
 import subprocess
 from pathlib import Path
 from typing import List, Tuple
@@ -13,6 +14,9 @@ class FailedToSlice(Exception):
 	pass
 
 class FailedToConcat(Exception):
+	pass
+
+class FailedToCleanUp(Exception):
 	pass
 
 
@@ -39,31 +43,53 @@ def slice_video(TEMP_DIR: Path, vslice: VideoSlice, i: int=0) -> Path:
 	return tmpfile
 
 
-def concat_video(TEMP_DIR: Path, stage_id: str, slice_paths: List[str]):
+def concat_video(TEMP_DIR: Path, stage_id: str, slice_paths: List[str]) -> Path:
 	# first create file list in temp dir
-	with open(str(TEMP_DIR / f""))
+	list_path = TEMP_DIR / f"concat-{stage_id}.txt"
+	with open(str(list_path), "w") as f:
+		pass
 
 	# then do subprocess for concat list
-
+	concat_path = TEMP_DIR / f"concat-{stage_id}.mp4"
 	cmd = [
 		"ffmpeg", "-f", "concat",
-		"-safe", "0", "-i", "mylist.txt",
-		"-c", "copy", "output.mp4"
+		"-safe", "0", "-i", str(list_path),
+		"-c", "copy", str(concat_path)
 	]
+	
+	# we need to hop directories real fast to perform this function.
+	# we'll come back when we're done.
+	cwd = os.getcwd()
+	os.chdir(str(TEMP_DIR))
 
-	return
+	result = subprocess.run(cmd)
+
+	if result.returncode != 0:
+		#cprint(f"#r#fRSkipping stage `{vslice.video_id}` due to error.#r\n")
+		raise FailedToConcat
+
+	os.chdir(cwd)
+
+	return concat_path
 
 
 def process_stage(conf: dict, stage: StageData) -> Tuple[Path]:
 	# slice all the slices
 	slice_paths = [slice_video(conf, stage.slices[x], x) for x in range(len(stage.slices))]
 
-	# TODO: edge case of one video?
+	# edge case of one video
+	if len(slice_paths) == 1:
+		return (slice_paths[0], slice_paths)
 
 	# concat all the slices
 	concat_path = concat_video(conf, stage.id, slice_paths)
 
 	# clean up old slices
+	for path in slice_paths:
+		try:
+			os.remove(path)
+		except Exception as e:
+			raise FailedToCleanUp(e)
 
 	# return the path of the concated vid
 	return (concat_path, slice_paths)
