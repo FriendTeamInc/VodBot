b2d6ca746c84ac3555b68dd269d11f5732f50d92
Logan H-D <notquiteapex@gmail.com>
2022-05-20 00:41:32 -0400

    add more fields, more comments

--- vodbot/config.py
+++ vodbot/config.py
@@ -1,6 +1,7 @@
 # Dedicated module for the config class and associated classes
 # having a dedicated type makes accessing specific members easier and dictated
 
+from typing import Dict, Literal
 from pathlib import Path
 
 # class ChannelThumbnailIcon:
@@ -43,24 +44,65 @@ class _ConfigPull:
 		# possible. Currently not implemented in any form and does not affect anything. This would
 		# not allow for downloading VODs under an authorized Client ID, or any sort of undocumented
 		# or nonexistant API access, still requiring GQL access in these cases.
-		self.api_use_alt = False
-		self.api_client = ""
-		self.api_secret = ""
+		# TODO?
+		#self.api_use_alt = False
+		#self.api_client = ""
+		#self.api_secret = ""
+
+class _ConfigChat():
+	def __init__(self,
+		export_format:Literal["raw","RealText","SAMI","YTT"]="RealText",
+		message_display_time:int=10,
+		randomize_uncolored_names:bool=True,
+	) -> None:
+		# Dictates what closed caption format the chat logs should be exported to when exporting.
+		self.export_format = export_format
+		# Dictates how long a single message should appear in the closed caption export.
+		self.message_display_time = message_display_time
+		# Toggle for giving white names (uncolored) a random color.
+		self.randomize_uncolored_names = randomize_uncolored_names
+
+		# YouTube Timed Text formatting options TODO
+		self.ytt_align = "left"
+		self.ytt_position_weight = 6
+		self.ytt_position_x = 0
+		self.ytt_position_y = 100
 
 class _ConfigStage():
-	def __init__(self) -> None:
-		pass
+	def __init__(self,
+		description_macros:Dict[str, str]={},
+		timezone:str="+0000"
+	) -> None:
+		# A UTC timezone code string, like "+0000" (GMT), "-0500" (EDT) or "+0930" (ACST). Used for
+		# calculating certain dates relating to videos (which store their date as ISO 8601).
+		self.timezone = timezone
+
+		# A dictionary of keys and related strings to make typing descriptions for stages easier,
+		# such as adding a lot of social media links at the end of a YouTube description.
+		self.description_macros = description_macros
 
 class _ConfigExport():
-	def __init__(self) -> None:
-		pass
+	def __init__(self,
+		ffmpeg_loglevel:Literal["warning","error","fatal"]="warning",
+		chat_enable:bool=True
+	) -> None:
+		# This is used to describe to FFMPEG what type of output there should be regarding when
+		# the program directs it to manage video files. "warning" is recommended as it displays very
+		# little unless otherwise necessary.
+		self.ffmpeg_loglevel = ffmpeg_loglevel
+
+		# A simple toggle for managing whether chat is exported with a video. More options are
+		# available in the chat config section.
+		self.chat_enable = chat_enable
+
+		# Hardware acceleration options
+		# TODO?
 
 class _ConfigUpload():
 	def __init__(self) -> None:
 		pass
 
 class _ConfigThumbnail():
-	
 	def __init__(self) -> None:
 		pass
 
@@ -74,6 +116,12 @@ class _ConfigDirectories():
 		temp:str, stage:str,
 		thumbnail:str
 	) -> None:
+		# Each key relates to a specific directory. `vod` is for storing VOD files, with metadata
+		# and chat (as applicable). `clip` is the same as the previous. Both are recommended to be
+		# on slow, dense storage devices `temp` is a working directory where video data is sliced,
+		# spliced, and processed, recommended to be on a high speed storage device. `stage` is a
+		# directory for storing information about to-be-processed files. `thumbnail` is for storing
+		# lots of images used for generating thumbnails for processed videos.
 		self.vod = Path(vod)
 		self.clip = Path(clip)
 		self.temp = Path(temp)
