dc4ee0e60efeafe33e82fd19e1f10316b883de3f
Logan H-D <notquiteapex@gmail.com>
2022-05-26 00:01:45 -0400

    Add a bunch of checks for config, remove old config stuff from util, and add example config in assets

--- vodbot/config.py
+++ vodbot/config.py
@@ -1,7 +1,6 @@
 # Dedicated module for the config class and associated classes
 # having a dedicated type makes accessing specific members easier and dictated
 
-from cgitb import enable
 from typing import Dict, List, Literal, Tuple
 from pathlib import Path
 
@@ -13,25 +12,21 @@ from pathlib import Path
 
 class _ConfigChannel:
 	def __init__(self,
-		name:str, username:str,
+		username:str="",
 		save_vods:bool=True, save_clips:bool=True, save_chat:bool=True,
-		# thumbnail_icon:ConfigChannelThumbnailIcon=None
-	) -> None:
-		self.name = name
+	**kwargs) -> None:
 		self.username = username
 		
 		self.save_vods = save_vods
 		self.save_clips = save_clips
 		self.save_chat = save_chat
 
-		# self.thumbnail_icon = thumbnail_icon
-
 class _ConfigPull:
 	def __init__(self,
-		save_chat:bool=True,
-		gql_client:str="kimne78kx3ncx6brgo4mv6wki5h1ko"
-	) -> None:
+		save_chat:bool=True, gql_client:str="kimne78kx3ncx6brgo4mv6wki5h1ko",
+	**kwargs) -> None:
 		# determines if chat logs get pulled with VODs and saved alongside metadata.
+		# is a master switch for every channel, if false then no chat gets saved.
 		self.save_chat = save_chat
 
 		# Client ID for accessing Twitch's public facing but privately documented GraphQL interface.
@@ -53,9 +48,8 @@ class _ConfigPull:
 class _ConfigChat():
 	def __init__(self,
 		export_format:Literal["raw","RealText","SAMI","YTT"]="RealText",
-		message_display_time:int=10,
-		randomize_uncolored_names:bool=True,
-	) -> None:
+		message_display_time:int=10, randomize_uncolored_names:bool=True,
+	**kwargs) -> None:
 		# Dictates what closed caption format the chat logs should be exported to when exporting.
 		self.export_format = export_format
 		# Dictates how long a single message should appear in the closed caption export.
@@ -71,9 +65,8 @@ class _ConfigChat():
 
 class _ConfigStage():
 	def __init__(self,
-		description_macros:Dict[str, str]={},
-		timezone:str="+0000"
-	) -> None:
+		description_macros:Dict[str, str]={}, timezone:str="+0000",
+	**kwargs) -> None:
 		# A UTC timezone code string, like "+0000" (GMT), "-0500" (EDT) or "+0930" (ACST). Used for
 		# calculating certain dates relating to videos (which store their date as ISO 8601).
 		self.timezone = timezone
@@ -85,98 +78,101 @@ class _ConfigStage():
 class _ConfigExport():
 	def __init__(self,
 		ffmpeg_loglevel:Literal["warning","error","fatal"]="warning",
-		chat_enable:bool=True
-	) -> None:
+		chat_enable:bool=True, video_enable:bool=True,
+	**kwargs) -> None:
 		# This is used to describe to FFMPEG what type of output there should be regarding when
 		# the program directs it to manage video files. "warning" is recommended as it displays very
 		# little unless otherwise necessary.
 		self.ffmpeg_loglevel = ffmpeg_loglevel
 
-		# A simple toggle for managing whether chat is exported with a video, if available. More
+		# A simple toggle for managing whether chat is exported with a stage, if available. More
 		# options are available in the chat config section.
 		self.chat_enable = chat_enable
+
+		# A simple toggle for managing whether video is exported with a stage. Useful for if you
+		# just need the chat logs.
+		self.video_enable = video_enable
 
 		# Hardware acceleration options
 		# TODO?
 
 class _ConfigUpload():
 	def __init__(self,
-		client_path:str, credentials_path:str,
-		chat_enable:bool=True
-	) -> None:
+		client_path:str, credentials_path:str, chat_enable:bool=True,
+	**kwargs) -> None:
 		# JSON files that contain important information relating to interfacing with YouTube.
-		self.client_path = client_path
-		self.credentials_path = credentials_path
+		self.client_path = client_path # Must exist for functionality
+		self.credentials_path = credentials_path # Must be accessible
 
 		# A simple toggle for managing whether chat is uploaded with a video, if available. More
 		# options are available in the chat config section. Only uploads YouTube Timed Text format.
 		self.chat_enable = chat_enable
 
-class _ConfigThumbnailHead():
-	pass
+# class _ConfigThumbnailIcon():
+# 	def __init__(self,
+# 		offset_x:int=0, offset_y:int=0,
+# 		scale:int=1, filepath:str="",
+# 	**kwargs) -> None:
+# 		self.offset_x = offset_x
+# 		self.offset_y = offset_y
+# 		self.scale = scale
+# 		self.filepath = filepath
 
-class _ConfigThumbnailGame():
-	pass
+# class _ConfigThumbnail():
+# 	def __init__(self,
+# 		enable:bool=False,
+# 		thumbnail_x:int=0, thumbnail_y:int=0,
+# 		thumbnail_width:int=1280, thumbnail_height:int=720,
+# 		thumbnail_filepath:str="",
+# 		screenshot_x:int=0, screenshot_y:int=0,
+# 		screenshot_width:int=1280, screenshot_height:int=720,
+# 		text_font:str="", text_gravity:str="NorthWest", text_size:int=160,
+# 		text_x:int=0, text_y:int=0,
+# 		heads:Dict[str, _ConfigThumbnailIcon]=None, head_order:List[int]=None,
+# 		head_positions:List[Tuple[int, int, int]]=None,
+# 		games:Dict[str, _ConfigThumbnailIcon]=None,
+# 		game_x:int=0,game_y:int=0,game_gravity:str="Center",
+# 	**kwargs) -> None:
+# 		pass # We'll do this later.
 
-class _ConfigThumbnail():
-	def __init__(self,
-		enable:bool=False,
-		thumbnail_x:int=0, thumbnail_y:int=0,
-		thumbnail_width:int=1280, thumbnail_height:int=720,
-		thumbnail_path:str="",
-		screenshot_x:int=0, screenshot_y:int=0,
-		screenshot_width:int=1280, screenshot_height:int=720,
-		font:str="", font_gravity:str="NorthWest", font_size:int=160,
-		font_x:int=0,font_y:int=0,
-		heads:List[_ConfigThumbnailHead]=None,
-		head_order:List[int]=None,
-		head_positions:List[Tuple[int,int]]=None,
-		games:List[_ConfigThumbnailGame]=None,
-		game_x:int=0,game_y:int=0,game_weight:str="Center"
-	) -> None:
-		pass
+# class _ConfigWebhookBase():
+# 	def __init__(self,
+# 		enable:bool=True, display_name:str=None, webhook_url:str=None,
+# 	**kwargs) -> None:
+# 		self.enable = enable
+# 		self.display_name = display_name
+# 		self.webhook_url = webhook_url
 
-class _ConfigWebhookBase():
-	def __init__(self,
-		enable:bool=False, display_name:str="VodBot",
-		webhook_url:str="", avatar_url:str=""
-	) -> None:
-		self.enable = enable
-		self.display_name = display_name
-		self.webhook_url = webhook_url
-		self.avatar_url = avatar_url
+# class _ConfigWebhooks():
+# 	def __init__(self,
+# 		enable:bool=False, display_name:str="VodBot", webhook_url:str="",
+# 		pull_vod:_ConfigWebhookBase=None,
+# 		pull_clip:_ConfigWebhookBase=None,
+# 		pull_job_done:_ConfigWebhookBase=None,
+# 		export_video:_ConfigWebhookBase=None,
+# 		export_job_done:_ConfigWebhookBase=None,
+# 		upload_video:_ConfigWebhookBase=None,
+# 		upload_job_done:_ConfigWebhookBase=None,
+# 	**kwargs) -> None:
+# 		pass # We'll do this later.
+# 		self.enable = enable
+# 		self.display_name = display_name
+# 		self.webhook_url = webhook_url
 
-class _ConfigWebhooks():
-	def __init__(self,
-		enable:bool=False, display_name:str="VodBot",
-		webhook_url:str="", avatar_url:str="",
-		pull_vod:_ConfigWebhookBase=None,
-		pull_clip:_ConfigWebhookBase=None,
-		pull_job_done:_ConfigWebhookBase=None,
-		export_video:_ConfigWebhookBase=None,
-		export_job_done:_ConfigWebhookBase=None,
-		upload_video:_ConfigWebhookBase=None,
-		upload_job_done:_ConfigWebhookBase=None,
-	) -> None:
-		self.enable = enable
-		self.display_name = display_name
-		self.webhook_url = webhook_url
-		self.avatar_url = avatar_url
-
-		self.pull_vod = pull_vod
-		self.pull_clip = pull_clip
-		self.pull_job_done = pull_job_done
-		self.export_video = export_video
-		self.export_job_done = export_job_done
-		self.upload_video = upload_video
-		self.upload_job_done = upload_job_done
+# 		self.pull_vod = pull_vod
+# 		self.pull_clip = pull_clip
+# 		self.pull_job_done = pull_job_done
+# 		self.export_video = export_video
+# 		self.export_job_done = export_job_done
+# 		self.upload_video = upload_video
+# 		self.upload_job_done = upload_job_done
 
 class _ConfigDirectories():
 	def __init__(self,
 		vod:str, clip:str,
 		temp:str, stage:str,
-		thumbnail:str
-	) -> None:
+		thumbnail:str,
+	**kwargs) -> None:
 		# Each key relates to a specific directory. `vod` is for storing VOD files, with metadata
 		# and chat (as applicable). `clip` is the same as the previous. Both are recommended to be
 		# on slow, dense storage devices `temp` is a working directory where video data is sliced,
@@ -190,15 +186,48 @@ class _ConfigDirectories():
 		self.thumbnail = Path(thumbnail)
 
 class Config:
-	def __init__(self, **kwargs) -> None:
-		self.channels = [] # channels to watch for new clips and videos
-		self.pull = _ConfigPull()
+	def __init__(self, conf:dict) -> None:
+		self.channels = {} # channels to watch for new clips and videos
+		_check_required_type(conf, "channels", dict)
+		for name, settings in conf["channels"].items():
+			_check_required_type(settings, "channel", str)
+			_check_optional_type(settings, "save_chat", bool)
+			_check_optional_type(settings, "save_vods", bool)
+			_check_optional_type(settings, "save_clips", bool)
+			self.channels[name] = _ConfigChannel(**settings)
+		
+		# is actually optional
+		if _check_required_type(conf, "pull", dict):
+			pull = conf["pull"]
+			_check_optional_type(pull, "save_chat", bool)
+			_check_optional_type(pull, "gql_client_id", str)
+			_check_optional_type(pull, "api_use_alt", bool)
+			_check_optional_type(pull, "api_client", str)
+			_check_optional_type(pull, "api_secret", str)
+			self.pull = _ConfigPull(**pull)
+		else:
+			self.pull = _ConfigPull()
+
 		self.stage = _ConfigStage()
 		self.export = _ConfigExport()
 		self.upload = _ConfigUpload()
-		self.thumbnail = _ConfigThumbnail()
-		self.webhooks = _ConfigWebhooks()
+
+		#self.thumbnail = _ConfigThumbnail()
+
+		#self.webhooks = _ConfigWebhooks()
+
 		self.directories = _ConfigDirectories()
-	
-	def load_json() -> None:
-		pass
+
+# Errors if key does not exist or is not the correct type.
+def _check_required_type(conf:dict, key:str, ttype:type):
+	if key not in conf:
+		pass # "required key value pair not in config, needed {key}."
+	else:
+		return _check_optional_type(conf, key, ttype)
+
+# Errors if key is not the correct type, but not required to exist.
+def _check_optional_type(conf:dict, key:str, ttype:type):
+	if key in conf and not isinstance(conf[key], ttype):
+		pass # "key does not match desired type of {ttype}, got {type(conf[key])}."
+
+	return True
