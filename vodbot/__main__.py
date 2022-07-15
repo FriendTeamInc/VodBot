ee29a95aacd3634f6cd68d5f33836d8dc425f2e3
Logan H-D <notquiteapex@gmail.com>
2022-07-15 13:14:30 -0400

    have init only create config, no input. remove some color and unnecessary args and text

--- vodbot/__main__.py
+++ vodbot/__main__.py
@@ -1,4 +1,4 @@
-from . import util, __project__, __version__
+from . import util, config, __project__, __version__
 from .printer import colorize
 
 import argparse
@@ -22,51 +22,33 @@ def deffered_main():
 
 
 def main():
-	titletext = colorize('#r#fM#l* {} {} (c) 2020-21 Logan "NotQuiteApex" Hickok-Dickson *#r')
-	titletext = titletext.format(__project__, __version__)
-	
-	print(titletext)
+	titletext = colorize(f'#r#fM#l* {__project__} {__version__} (c) 2020-22 Logan "NotQuiteApex" Hickok-Dickson *#r')
 
 	# Process arguments
-	parser = argparse.ArgumentParser(epilog=titletext,
-		description="Downloads and processes VODs and clips from Twitch.tv channels.")
+	parser = argparse.ArgumentParser(description="Downloads and processes VODs and clips from Twitch.tv channels.")
 	parser.add_argument("-v","--version", action="version", version=titletext)
 	parser.add_argument("-c","--config", type=Path, dest="config",
-		help="location of the Twitch config file to use", default=util.DEFAULT_CONF_PATH)
+		help="location of the Twitch config file to use", default=config.DEFAULT_CONFIG_PATH)
 
 	# Subparsers for different commands
 	subparsers = parser.add_subparsers(title="command", dest="cmd", help="command to run.")
 
 	# `vodbot init`
-	initparse = subparsers.add_parser("init", epilog=titletext,
+	initparse = subparsers.add_parser("init",
 		description="Runs the setup process for VodBot")
-	initparse.add_argument("--output", help="path to save the config to; defaults to ~/.vodbot/conf.json",
-		type=Path, required=False, default=util.DEFAULT_CONF_PATH, dest="output", metavar="PATH")
-	initparse.add_argument("--default", action="store_true",
-		help="argument switch to write default config; overrides all subsequent options")
-	initparse.add_argument("--channels", type=str, default=[], nargs="+",
-		help="twitch.tv channel login to pull VODs/Clips from", metavar="CHL")
-	initparse.add_argument("--timezone", type=str, required=False, default=None,
-		help="timezone for datetime referencing, as a UTC string")
-	initparse.add_argument("--vod-dir", help="absolute path to store VOD data",
-		type=Path, required=False, default=None, dest="voddir", metavar="PATH")
-	initparse.add_argument("--clip-dir", help="absolute path to store Clip data",
-		type=Path, required=False, default=None, dest="clipdir", metavar="PATH")
-	initparse.add_argument("--temp-dir", help="absolute path to store temporary data",
-		type=Path, required=False, default=None, dest="tempdir", metavar="PATH")
-	initparse.add_argument("--stage-dir", help="absolute path to store staged data",
-		type=Path, required=False, default=None, dest="stagedir", metavar="PATH")
+	initparse.add_argument("-o", "--output", help="path to save the config to",
+		type=Path, default=config.DEFAULT_CONFIG_PATH, dest="output", metavar="PATH")
 
 	# `vodbot pull <vods/clips/both> [channel ...]`
 	download = subparsers.add_parser("download", aliases=["pull", "download"],
-		epilog=titletext, description="Downloads VODs and/or clips.")
+		description="Downloads VODs and/or clips.")
 	download.add_argument("type", type=str, default="both", nargs="?",
 		help='content type flag, can be "vods", "clips", or "both"; defaults to "both"')
 	download.add_argument("channels", metavar="channel", type=str, default=[], nargs="*",
 		help="twitch.tv channel name to pull VODs from; optional, defaults to config")
 
 	# `vodbot stage`
-	stager = subparsers.add_parser("stage", epilog=titletext,
+	stager = subparsers.add_parser("stage",
 		description="Stages sections of video to upload or export",)
 	stager_subparser = stager.add_subparsers(title="action", dest="action",
 		description='action for staging the video')
@@ -77,43 +59,43 @@ def main():
 	# `<other_id> [--ss "0:20:0"] [--to "2:59:06"] \`
 	# `<other_id> [--ss "3:20:0"] [--to "4:59:06"] \`
 	# `<also_id> [--ss "0:40:0"] [--to "6:59:59"]`
-	stager_add = stager_subparser.add_parser("new", epilog=titletext,
+	stager_add = stager_subparser.add_parser("new",
 		description="creates a new stage for videos and clips to be mixed")
 	stager_add.add_argument("id", help="id of the VOD or Clip to stage",
 		type=str, nargs="+")
 	stager_add.add_argument("--title", help="title of finished video",
-		type=str, required=False, default="")
+		type=str, default="")
 	stager_add.add_argument("--desc", help="description of finished video",
-		type=str, required=False, default="")
+		type=str, default="")
 	stager_add.add_argument("--ss", help="start time of video",
-		type=str, required=False, default=[], nargs="?", action="append")
+		type=str, default=[], nargs="?", action="append")
 	stager_add.add_argument("--to", help="end time of video",
-		type=str, required=False, default=[], nargs="?", action="append")
+		type=str, default=[], nargs="?", action="append")
 
 	# `vodbot stage rm <id>`
-	stager_rm = stager_subparser.add_parser("rm", epilog=titletext,
+	stager_rm = stager_subparser.add_parser("rm",
 		description="removes a VOD or Clip from the staging area")
 	stager_rm.add_argument("id", type=str, help="id of the staged video data")
 
 	# `vodbot stage list [id]`
-	stager_list = stager_subparser.add_parser("list", epilog=titletext,
+	stager_list = stager_subparser.add_parser("list",
 		description="lists info on staging area or staged items")
 	stager_list.add_argument("id", nargs="?", type=str, help="id of the staged video data", default=None)
 
 	# `vodbot upload <id/all>`
 	upload = subparsers.add_parser("upload", aliases=["upload", "push"],
-		epilog=titletext, description="Uploads stage(s) to YouTube.")
+		description="Uploads stage(s) to YouTube.")
 	upload.add_argument("id", type=str,
 		help='id of the staged video data, "all" to upload all stages, or "logout" to remove existing YouTube credentials')
 	
 	# `vodbot export <id/all>`
 	export = subparsers.add_parser("export", aliases=["export", "slice"],
-		epilog=titletext, description="Uploads stage(s) to YouTube.")
+		description="Uploads stage(s) to YouTube.")
 	export.add_argument("id", type=str, help="id of the staged video data, or `all` for all stages")
 	export.add_argument("path", type=Path, help="directory to export the video(s) to")
 
 	# `vodbot upload <id/all>`
-	info = subparsers.add_parser("info", epilog=titletext,
+	info = subparsers.add_parser("info",
 		description="Prints out info on the Channel, Clip, or VOD given.")
 	info.add_argument("id", type=str, help="id/url of the Channel, Clip, or VOD")
 	
