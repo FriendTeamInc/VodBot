686903cfdb2739abcb6507652813d1b21f47ffce
Logan Hickok-Dickson <self@notquiteapex.net>
2022-08-14 16:09:26 -0400

    finish completer for videos as well as update stage completer to add commands for uploads

--- vodbot/__main__.py
+++ vodbot/__main__.py
@@ -1,3 +1,5 @@
+from ast import parse
+from cmath import log
 from . import util, __project__, __version__
 from .config import DEFAULT_CONFIG_PATH
 from .printer import cprint, colorize
@@ -15,8 +17,27 @@ from os.path import isfile as os_isfile
 def video_completer(prefix, parsed_args, **kwargs):
 	# searches all vod and clip directories for meta files, and strips down the names to just the ID's
 	conf = util.load_conf(parsed_args.config)
+
+	allvids = []
+
+	for channel in conf.channels:
+		login = channel.username
+
+		voddir = conf.directories.vods / login
+		vods = [ d.split("_")[1][:-5] 
+			for d in os_listdir(voddir)
+				if os_isfile(voddir / d) and d.endswith(".meta")
+		]
+		clipdir = conf.directories.clips / login
+		clips = [ d.split("_")[1][:-5] 
+			for d in os_listdir(clipdir)
+				if os_isfile(clipdir / d) and d.endswith(".meta")
+		]
+
+		allvids += [d for d in vods if d.startswith(prefix)]
+		allvids += [d for d in clips if d.startswith(prefix)]
 	
-	pass
+	return allvids
 
 
 def stage_completer(prefix, parsed_args, **kwargs):
@@ -26,10 +47,20 @@ def stage_completer(prefix, parsed_args, **kwargs):
 	
 	stages = [ d[:-6]
 		for d in os_listdir(stagedir)
-			if os_isfile(stagedir / d) and d[-5:] == "stage" and d.startswith(prefix)
+			if os_isfile(stagedir / d) and d.endswith(".stage") and d.startswith(prefix)
 	]
+
+	cmd = parsed_args.cmd	
+
+	addall = []
+	if (cmd == "push" or cmd == "upload") and "all".startswith(prefix):
+		addall = ["all"]
+
+	addlogout = []
+	if (cmd == "push" or cmd == "upload") and "logout".startswith(prefix):
+		addlogout = ["logout"]
 	
-	return stages
+	return stages + addall + addlogout
 
 
 def deffered_main():
@@ -48,13 +79,14 @@ def main():
 	# Process arguments
 	parser = argparse.ArgumentParser(description="Downloads and processes VODs and clips from Twitch.tv channels.")
 	parser.add_argument("-v","--version", action="version", version=titletext)
-	parser.add_argument("-c","--config", type=Path, dest="config", default=DEFAULT_CONFIG_PATH,
-		help="location of the Twitch config file to use").completer = FilesCompleter
 	parser.add_argument("-n", "--no-color", action="store_true", dest="color_toggle", default=False,
 		help="disables colorful output of the program")
+	parser.add_argument("-c","--config", type=Path, dest="config", metavar="CFG", default=DEFAULT_CONFIG_PATH,
+		help="location of the config file to use").completer = FilesCompleter
 
 	# Subparsers for different commands
-	subparsers = parser.add_subparsers(title="command", dest="cmd", help="command to run.")
+	subparsers = parser.add_subparsers(title="command", dest="command", metavar="CMD",
+		help="command to run: init, info, pull, stage, or upload.")
 
 	# `vodbot init`
 	initparse = subparsers.add_parser("init", description="Runs the setup process for VodBot")
@@ -62,15 +94,15 @@ def main():
 		help="path to save the config to").completer = FilesCompleter
 
 	# `vodbot pull <vods/clips/both>`
-	download = subparsers.add_parser("pull", description="Downloads VODs and/or clips.")
+	download = subparsers.add_parser("pull", aliases=["download"], description="Downloads VODs and/or clips.")
 	download.add_argument("type", type=str, default="both", nargs="?", choices=("vods", "clips", "both"),
 		help='what type of content to pull, can be "vods", "clips", or "both"')
 
 	# `vodbot stage`
 	stager = subparsers.add_parser("stage",
 		description="Stages sections of video to upload or export",)
-	stager_subparser = stager.add_subparsers(title="action", dest="action",
-		description='action for staging the video')
+	stager_subparser = stager.add_subparsers(title="action", dest="action", metavar="ACT"
+		description='action for video stages: new, list, or rm.')
 
 	# `vodbot stage new \
 	# `[--title "Apex - BBT"] [--desc "PogChamp {streamer}\n{link}"] \`
@@ -79,7 +111,7 @@ def main():
 	# `<other_id> [--ss "3:20:0"] [--to "4:59:06"] \`
 	# `<also_id> [--ss "0:40:0"] [--to "6:59:59"]`
 	stager_add = stager_subparser.add_parser("new", description="creates a new stage for videos and clips to be mixed")
-	stager_add.add_argument("id", help="id of the VOD or Clip to stage", type=str, nargs="+")
+	stager_add.add_argument("id", help="id of the VOD or Clip to stage", type=str, nargs="+").completer = video_completer
 	stager_add.add_argument("--title", help="title of finished video", type=str, default="")
 	stager_add.add_argument("--desc", help="description of finished video", type=str, default="")
 	stager_add.add_argument("--ss", help="start time of video", type=str, default=[], nargs="?", action="append")
@@ -95,7 +127,7 @@ def main():
 		nargs="?", default=None).completer = stage_completer
 
 	# `vodbot upload <stage_id/all>`
-	upload = subparsers.add_parser("push", description="Uploads stage(s) to YouTube.")
+	upload = subparsers.add_parser("push", aliases=["upload"], description="Uploads stage(s) to YouTube.")
 	upload.add_argument("id", type=str,
 		help='id of the staged video data, "all" to upload all stages, or "logout" to remove existing YouTube credentials').completer = stage_completer
 	
